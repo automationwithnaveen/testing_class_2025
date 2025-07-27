510834d6dbc8b3623e95330434052a4ebacff7f5
automationwithnaveen <automationwithnaveen@gmail.com>
2025-07-27 09:50:33 +0100

    changes

--- Project2/src/com/example_one/HelloWorld.java
+++ Project2/src/com/example_one/HelloWorld.java
@@ -1,16 +1,33 @@
 package com.example_one;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
+
 
 public class HelloWorld 
 {
+	public static String given_title = "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
+	
 	 public static void main(String[] args)
 	 {
-	        WebDriver driver = new FirefoxDriver();
+	        WebDriver driver = new ChromeDriver();
 
-	        driver.get("https://www.google.com/");
+	        driver.get("https://www.flipkart.com/");
+	        
+	        String title = driver.getTitle();
+	        
+	        System.out.println(title);
+	        
+	        if(given_title.equals(title))
+	        {
+	        	System.out.println("Pass:Valid Home Page");
+	        }
+	        else
+	        {
+	        	System.out.println("Fail:In-Valid Home Page");
+	        }
+	        
+	        //System.out.println(title);
 
 	        //driver.quit();
 	 }
